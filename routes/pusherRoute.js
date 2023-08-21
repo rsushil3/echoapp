4f17fe24c76ddf7ffe8b7366982d76ad2f77186a
rsushil3 <142304427+rsushil3@users.noreply.github.com>
2023-08-21 15:46:18 +0530

    Update pusherRoute.js

--- routes/pusherRoute.js
+++ routes/pusherRoute.js
@@ -1,14 +1,10 @@
 import express from "express";
 import {
-  authController,
   messagesController,
 } from "../controllers/pusherController.js";
 
 //router object
 const router = express.Router();
-
-//Pusher Auth || POST
-router.post('/auth', authController);
 
 //Send Messages to pusher API || POST
 router.post("/messages/:chatId", messagesController);
