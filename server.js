6d96ab65471ddef5fd5a4fc4b115e6f1717656e4
rsushil3 <142304427+rsushil3@users.noreply.github.com>
2023-08-21 15:38:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import pusherRoutes from "./routes/pusherRoute.js";
+import Pusher from 'pusher'; // Import the Pusher library
 import cors from "cors";
 import path from 'path';
 import {fileURLToPath} from 'url';
+
+//PORT
+const PORT = process.env.PORT || 8000;
 
 //configure env
 dotenv.config();
@@ -29,13 +33,28 @@ app.use(express.static(path.join(__dirname, './client/build')))
 app.use("/api/auth", authRoutes);
 app.use("/api/pusher", pusherRoutes);
 
+// Pusher configuration
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true
+});
+
+app.post('/pusher/auth', (req, res) => {
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const auth = pusher.authorizeChannel(socketId, channel);
+  res.send(auth);
+});
+
 //rest api
 app.use("*", function(req, res){
   res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
-//PORT
-const PORT = process.env.PORT || 8000;
+
 
 //run listen
 app.listen(PORT, () => {
