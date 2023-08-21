136c21a6c85c46993e50d2990a97b9a0c052478b
rsushil3 <142304427+rsushil3@users.noreply.github.com>
2023-08-21 14:58:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
-import Pusher from 'pusher'; // Import the Pusher library
+// import Pusher from 'pusher'; // Import the Pusher library
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
@@ -26,31 +26,31 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// Pusher configuration
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
-  useTLS: true
-});
+// // Pusher configuration
+// const pusher = new Pusher({
+//   appId: process.env.PUSHER_APP_ID,
+//   key: process.env.PUSHER_KEY,
+//   secret: process.env.PUSHER_SECRET,
+//   cluster: process.env.PUSHER_CLUSTER,
+//   useTLS: true
+// });
 
-app.post('/pusher/auth', (req, res) => {
-  const socketId = req.body.socket_id;
-  const channel = req.body.channel_name;
-  const auth = pusher.authorizeChannel(socketId, channel);
-  res.send(auth);
-});
+// app.post('/pusher/auth', (req, res) => {
+//   const socketId = req.body.socket_id;
+//   const channel = req.body.channel_name;
+//   const auth = pusher.authorizeChannel(socketId, channel);
+//   res.send(auth);
+// });
 
-app.post('/messages/:chatId', (req, res) => {
-  const chatId = req.params.chatId;
-  const { sender, content } = req.body;
-  // Trigger an event on the Pusher channel
-  pusher.trigger(`private-${chatId}`, 'client-receive-message', {
-    sender,
-    content
-  });
-});
+// app.post('/messages/:chatId', (req, res) => {
+//   const chatId = req.params.chatId;
+//   const { sender, content } = req.body;
+//   // Trigger an event on the Pusher channel
+//   pusher.trigger(`private-${chatId}`, 'client-receive-message', {
+//     sender,
+//     content
+//   });
+// });
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
